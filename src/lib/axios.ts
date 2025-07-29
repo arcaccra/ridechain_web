a45aeb17d6d07ceed22434fb54e93b913a95710d
tetawiah <tawiah.tetteh10@gmail.com>
2025-07-29 13:40:31 +0000

    added axios instance with interceptors for API requests; introduced toast hook and Toaster component for improved notifications; updated dependencies including @tanstack/react-query, axios, next-themes, and sonner for enhanced feature support

---
+++ src/lib/axios.ts
@@ -0,0 +1,54 @@
+
+import axios from "axios";
+
+// Create an instance of Axios
+const httpFetch = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        Accept: "application/json",
+    },
+});
+
+// Add a request interceptor
+httpFetch.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        // Handle custom content types dynamically
+        if (!config.headers["Content-Type"]) {
+            if (config.data instanceof FormData) {
+                config.headers["Content-Type"] = "multipart/form-data";
+            } else {
+                config.headers["Content-Type"] = "application/json";
+            }
+        }
+
+        return config;
+    },
+    (error) => {
+
+        return Promise.reject(error);
+    }
+);
+
+
+httpFetch.interceptors.response.use(
+    (response) => response,
+    (error) => {
+
+        if (error.response) {
+            if (error.response.status === 401) {
+                window.location.href = "/login";
+            }
+            console.error("API Error:", error.response.data.message || error.message);
+        } else {
+            console.error("Network Error:", error.message);
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default httpFetch;
